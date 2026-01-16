d6e9e08b91fe7f1073ade0bd3cdbfbee2c602628
stevenmarp <stevenmarp@sinerka.id>
2026-01-16 23:33:10 +0700

    SM Calculator Widget v16.0.1.0.0 - Initial release for Odoo 16.0
    Features:
    - Calculator widget in systray for quick calculations
    - Keyboard support for number input
    - Memory functions (MC, MR, M+, M-)
    - Clear/All Clear functionality
    - Modern responsive design
    - Smooth animations and visual feedback
    
    Compatible with Odoo 16.0
    License: OPL-1

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Calculator Widget',
-    'version': '18.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Productivity',
     'summary': 'Floating calculator widget in systray for quick calculations',
     'description': """
